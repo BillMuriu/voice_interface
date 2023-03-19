72bc699d0ccec58c2c77b3bdded576cd01681abf
unknown <njorogekamau63@gmail.com>
2023-03-19 17:16:44 +0300

    a

--- nl_interface/analytics/urls.py
+++ nl_interface/analytics/urls.py
@@ -1,10 +1,10 @@
-from django.contrib import admin
 from django.urls import path
+from . import views
 
 
 from django.http import HttpResponse
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
 ]
